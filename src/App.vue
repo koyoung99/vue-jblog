21b738f70627047d69a3a151eeefd2011e36557a
김아영 <72481734+koyoung99@users.noreply.github.com>
2024-04-09 17:18:55 +0900

    20240409

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <router-view />
+</template>
+<script>
+import "@/assets/css/jblog.css";
+export default {
+
+}
+</script>
---
+++ src/views/main/MainView.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="center-content">
+
+        <!--메인 해더 자리 -->
+
+
+        <form id="search-form">
+            <fieldset>
+                <input type="text" name="keyword">
+                <button id="btnSearch" type="submit">검색</button>
+            </fieldset>
+
+            <fieldset>
+                <label for="rdo-title">블로그 제목</label>
+                <input id="rdo-title" type="radio" name="kwdOpt" value="optTitle">
+
+                <label for="rdo-userName">블로거 이름</label>
+                <input id="rdo-userName" type="radio=" name="kwdOpt" value="optName">
+            </fieldset>
+        </form>
+
+        <div id="resultList">
+
+
+        </div>
+
+        <!-- 메인 푸터  자리-->
+
+
+    </div>
+    <!-- //center-content -->
+
+</template>
+
+<script>
+export default {
+    name: "MainView",
+    components: {},
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
+
+<style></style>
